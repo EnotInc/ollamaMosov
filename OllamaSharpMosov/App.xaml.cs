db3cbeefc147767ee1f1b8b496688c93ef6a715e
EnotInc <53831339+EnotInc@users.noreply.github.com>
2025-02-13 21:30:31 +0300

    seccond commit

---
+++ OllamaSharpMosov/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace OllamaSharpMosov
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
---
+++ OllamaSharpMosov/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using OllamaSharp;
+
+namespace OllamaSharpMosov
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();        }
+
+        public async Task ollama(string message)
+        {
+            if (string.IsNullOrEmpty(message))
+            {
+                MessageBox.Show("Enter your question");
+                return;
+            }
+            try
+            {
+                var uri = new Uri("http://localhost:11434");
+                var ollama = new OllamaApiClient(uri);
+
+                ollama.SelectedModel = "llama2";
+                var models = await ollama.ListLocalModelsAsync();
+
+                var chat = new Chat(ollama);
+                string moderRole = "Ты мастер по определению видов цветов и уходу за ними";
+
+                await foreach (var answerToken in chat.SendAsync($"{moderRole}, {message}"))
+                {
+                    answerTB.Text = $"{answerToken} 123123";
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(""+ex);
+            }
+        }
+
+        private async void askOllama(object sender, RoutedEventArgs e)
+        {
+            await ollama(questionTB.Text);
+            questionTB.Text = "";
+        }
+    }
+}
